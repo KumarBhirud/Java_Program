36fc694bb74b9fa1adb240d670b50bbea9e87fe7
Kumar Bhirud <bhirudkumar@gmail.com>
2025-10-04 13:22:39 +0530

    sencond commit statements

--- src/Day_01/Hello_World.java
+++ src/Day_01/Hello_World.java
@@ -1,4 +1,9 @@
 package Day_01;
 
 public class Hello_World {
+ //   static void main(String[] args) {
+ static void main() {
+     System.out.println("Hello_World");
+     System.out.println("Test 01");
+   }
 }
