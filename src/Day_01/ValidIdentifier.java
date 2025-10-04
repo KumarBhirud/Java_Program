b354391b295d8fc647fab7c6d43a1061a12e3e2c
Kumar Bhirud <bhirudkumar@gmail.com>
2025-10-04 23:34:09 +0530

    commit 2nd pkg

---
+++ src/Day_01/ValidIdentifier.java
@@ -0,0 +1,18 @@
+package Day_01;
+
+public class ValidIdentifier {
+ static void main(String[] args) {
+        System.out.println("Test01");
+        String ab ="abvf";
+
+    }
+    void  M2(){
+        System.out.println("test");
+    }
+
+    void a12a(){
+
+        System.out.println("methods NPNR");
+    }
+}
+
