4b79b7c014139f9aa7513b2901fa9c0b9e648861
Kumar Bhirud <bhirudkumar@gmail.com>
2025-10-04 23:31:41 +0530

    commit 2nd pkg

---
+++ src/Day_02_Java_basics/Lab001_Prinf.java
@@ -0,0 +1,27 @@
+package Day_02_Java_basics;
+
+import java.util.Scanner;
+
+public class Lab001_Prinf {
+
+    static void main(String[] args) {
+        int number =5;
+
+            // %d is user to decimal integer
+            // %n is used to add new line
+
+        System.out.printf("%d*1=%d%n", number, number * 1);
+        System.out.printf("%d*2=%d%n", number, number * 2);
+        System.out.printf("%d*3=%d%n", number, number * 3);
+        System.out.printf("%d*4=%d%n", number, number * 4);
+        System.out.printf("%d*5=%d%n", number, number * 5);
+        System.out.printf("%d*6=%d%n", number, number * 6);
+        System.out.printf("%d*7=%d%n", number, number * 7);
+        System.out.printf("%d*8=%d%n", number, number * 8);
+        System.out.printf("%d*9=%d%n", number, number * 9);
+        System.out.printf("%d*10=%d%n", number, number * 10);
+
+
+
+    }
+}
