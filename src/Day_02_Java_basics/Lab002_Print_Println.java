8aeebaa2ea2aabe4175842ec3b29a061dd239505
Kumar Bhirud <bhirudkumar@gmail.com>
2025-10-04 23:29:55 +0530

    commit 2nd pkg

---
+++ src/Day_02_Java_basics/Lab002_Print_Println.java
@@ -0,0 +1,15 @@
+package Day_02_Java_basics;
+
+public class Lab002_Print_Println {
+    static void main(String[] args) {
+
+        System.out.print("Kumar");
+        System.out.print("Bhirud");   // print evrything in single line
+
+        System.out.println();   //Println add extra line in the end
+
+        System.out.println("Kumar");
+        System.out.println("Bhirud");
+
+    }
+}
